1eea34d2a5210c21de08e3e490f8156615d07256
atusy <30277794+atusy@users.noreply.github.com>
2023-08-14 19:01:09 +0900

    refactor: the bottom row of layer 2 inherits mappings of layer 1 and 0

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_universal(
-    KC_ESC  ,KC_1   , KC_2        , KC_3   , KC_4          , KC_5          ,                                 KC_6          , KC_7   , KC_8   , KC_9   , KC_0   , KC_HOME,
-    KC_LBRC, KC_Q   , KC_W        , KC_E   , KC_R          , KC_T          ,                                 KC_Y          , KC_U   , KC_I   , KC_O   , KC_P   , KC_RBRC,
-    KC_GRV , KC_A   , KC_S        , KC_D   , KC_F          , KC_G          ,                                 KC_H          , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-    KC_EQL , KC_Z   , KC_X        , KC_C   , KC_V          , KC_B          , KC_ESC       ,    KC_BSPC     , KC_N          , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
-    KC_LCTL, KC_LSFT, KC_MENU     , KC_LWIN, LALT_T(KC_ENT), LSFT_T(KC_SPC), LT(3,KC_BSLS),    LT(1,KC_TAB), RCTL_T(KC_ENT), _______, _______, _______, KC_BTN1, TT(3)
+    KC_ESC  ,KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                              KC_6    , KC_7   , KC_8   , KC_9   , KC_0   , KC_HOME,
+    KC_LBRC, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                              KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_RBRC,
+    KC_GRV , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                              KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    KC_EQL , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ESC ,            KC_BSPC, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
+    _______, _______, _______, _______, _______, _______, _______,            _______, _______ , _______, _______, _______, _______, _______
   ),
 
   [3] = LAYOUT_universal(
