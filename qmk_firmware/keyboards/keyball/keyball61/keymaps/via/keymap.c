f431341a74bd2192d75cd5c0e6a9e47e825872ee
atusy <30277794+atusy@users.noreply.github.com>
2024-09-19 00:40:10 +0900

    feat!(mapping): exchange lalt and lwin

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LBRC, KC_Q   , KC_W        , KC_E   , KC_R          , KC_T          ,                                 KC_Y          , KC_U   , KC_I   , KC_O   , KC_P   , KC_RBRC,
     KC_GRV , KC_A   , KC_S        , KC_D   , KC_F          , KC_G          ,                                 KC_H          , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_EQL , KC_Z   , KC_X        , KC_C   , KC_V          , KC_B          , KC_ESC       ,    KC_BSPC     , KC_N          , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
-    KC_LCTL, KC_LSFT, KC_MENU     , KC_LWIN, LALT_T(KC_ENT), LSFT_T(KC_SPC), LT(3,KC_BSLS),    LT(2,KC_TAB), RCTL_T(KC_ENT), _______, _______, _______, KC_BTN1, TT(3)
+    KC_LCTL, KC_LSFT, KC_MENU     , KC_LALT, LWIN_T(KC_ENT), LSFT_T(KC_SPC), LT(3,KC_BSLS),    LT(2,KC_TAB), RCTL_T(KC_ENT), _______, _______, _______, KC_BTN1, TT(3)
   ),
 
   [1] = LAYOUT_universal(
