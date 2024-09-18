3a010133ec12e488865520bfcdc52284bee77e81
atusy <30277794+atusy@users.noreply.github.com>
2024-09-19 00:54:58 +0900

    temp

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    _______, KC_PSCR, LT(3,KC_TAB), KC_BTN1, KC_BTN2       , KC_BTN3       ,                                 _______       , _______, _______, _______, _______, _______,
+    KC_A , KC_PSCR, LT(3,KC_TAB), KC_BTN1, KC_BTN2       , KC_BTN3       ,                                 _______       , _______, _______, _______, _______, _______,
     KC_LBRC, KC_Q   , KC_W        , KC_E   , KC_R          , KC_T          ,                                 KC_Y          , KC_U   , KC_I   , KC_O   , KC_P   , KC_RBRC,
     KC_GRV , KC_A   , KC_S        , KC_D   , KC_F          , KC_G          ,                                 KC_H          , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_EQL , KC_Z   , KC_X        , KC_C   , KC_V          , KC_B          , KC_ESC       ,    KC_BSPC     , KC_N          , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
